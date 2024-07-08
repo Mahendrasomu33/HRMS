08f29bf66ccea0f4fec1bcc3b6fbcf8f9d466f99
Mahendrasomu33 <mr.somu33@gmail.com>
2024-07-08 13:50:20 +0530

    form

--- collage/collage.js
+++ collage/collage.js
@@ -18,4 +18,23 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 
+
+  function toggleAccordion(element) {
+    const accordionHeader = element;
+    const accordionContent = accordionHeader.nextElementSibling;
+
+    if (accordionContent.style.display === "block") {
+        accordionContent.style.display = "none";
+        accordionHeader.classList.remove("active");
+    } else {
+        const allContents = document.querySelectorAll(".accordion-content");
+        const allHeaders = document.querySelectorAll(".accordion-header");
+
+        allContents.forEach(content => content.style.display = "none");
+        allHeaders.forEach(header => header.classList.remove("active"));
+
+        accordionContent.style.display = "block";
+        accordionHeader.classList.add("active");
+    }
+}
   
