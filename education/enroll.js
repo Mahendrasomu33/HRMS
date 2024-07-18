8090614efa14da448bb34dd3e5fbfc23d7217235
Mahendrasomu33 <mr.somu33@gmail.com>
2024-07-18 13:52:18 +0530

    jsPage and Enroll Page in Course

---
+++ education/enroll.js
@@ -0,0 +1,13 @@
+function showDetails(courseId) {
+    const details = document.getElementById(courseId);
+    if (details.style.display === 'none' || details.style.display === '') {
+        details.style.display = 'block';
+    } else {
+        details.style.display = 'none';
+    }
+}
+
+function enroll(courseName) {
+    alert(`You have enrolled in the ${courseName}!`);
+}
+ 
